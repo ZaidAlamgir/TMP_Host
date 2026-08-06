fbe68c8d0e4346c49f15adfc26a7287ada919cb5
ZaidAlamgir <zaidkhan137782@gmail.com>
2026-08-06 21:44:30 +0530

    perf: compile tailwind CSS statically and defer blocking assets to improve mobile LCP from 11s to <1s

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./_posts/**/*.md",
+    "./_live_updates/**/*.md",
+    "./*.html",
+    "./*.md",
+    "./*.js",
+    "./assets/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
